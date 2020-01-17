862760b15c4334d0fd286e1b84908f5d97a32d62
Haolin Jiang <hljiang@agiledigital.com.au>
2020-01-17 11:54:15 +1100

    Update node12BuilderContainerTemplate.groovy

--- vars/node12BuilderContainerTemplate.groovy
+++ vars/node12BuilderContainerTemplate.groovy
@@ -2,7 +2,7 @@ def call() {
   return [
     containerTemplate(
       name: 'node12-builder',
-      image: 'cypress/base:12.14',
+      image: 'cypress/base:12.14.0',
       alwaysPullImage: true,
       command: 'cat',
       ttyEnabled: true
